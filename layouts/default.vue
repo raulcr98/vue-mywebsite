67a82696a3e0979105c8deae5b146793b5f384f9
Raul C. Rivero <rahulrcr98@gmail.com>
2020-06-18 19:57:03 -0400

    add: Added suscription form

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,21 +20,73 @@
         <template slot="end">
           <b-navbar-item tag="div">
             <div class="buttons">
-              <nuxt-link class="button is-primary is-rounded" to="/addme">
+              <button
+                class="button is-primary is-rounded"
+                @click="active = true"
+              >
                 <strong>Suscríbete a mi boletín</strong>
                 <b-icon icon="mail" style="margin-left: 5pt"></b-icon>
-              </nuxt-link>
+              </button>
             </div>
           </b-navbar-item>
         </template>
       </b-navbar>
     </template>
+    <div :class="active ? 'modal is-active' : 'modal'">
+      <div class="modal-background"></div>
+      <div class="modal-content columns">
+        <div class="column"></div>
+        <section
+          class="hero is-white column is-three-fifths"
+          style="border-radius: 10pt"
+        >
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                Suscríbete a mi boletín
+              </h1>
+              <form action="">
+                <div class="field">
+                  <label class="label">Correo</label>
+                  <div class="control has-icons-left has-icons-right">
+                    <input class="input" type="email" placeholder="Correo" />
+                    <span class="icon is-small is-left">
+                      <b-icon icon="email"></b-icon>
+                    </span>
+                    <span class="icon is-small is-right">
+                      <i class="fas fa-exclamation-triangle"></i>
+                    </span>
+                  </div>
+                </div>
+                <div class="field is-grouped">
+                  <div class="control">
+                    <button class="button is-success">Suscríbete</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+        <div class="column"></div>
+      </div>
+      <button
+        class="modal-close is-large"
+        aria-label="close"
+        @click="active = false"
+      ></button>
+    </div>
     <nuxt />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      active: false
+    }
+  }
+}
 </script>
 
 <style scoped></style>
