63d02bd94e90513529dc5ca16ec2d0020592a773
Raul C. Rivero <rahulrcr98@gmail.com>
2020-06-18 18:53:33 -0400

    fix: Fixed layout issue

--- components/Banner.vue
+++ components/Banner.vue
@@ -5,7 +5,7 @@
         <h1 class="title is-1">Raúl C. Rivero</h1>
         <img class="photo" src="banner1.jpg" alt="" />
         <p class="pt">
-          Hola mi nombre es Raúl y soy un desarrollador web nacido en Cuba
+          Hola mi nombre es Raúl y soy desarrollador web nacido en Cuba
         </p>
         <div class="pt">
           <a v-for="social in socials" :key="social.name" :href="social.url">
