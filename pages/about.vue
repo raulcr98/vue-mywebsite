6b64b8b0a65f1e44fc113ff7aae650957f240572
Raul C. Rivero <rahulrcr98@gmail.com>
2020-06-23 00:39:08 -0400

    perf: Improve about content

--- pages/about.vue
+++ pages/about.vue
@@ -20,9 +20,17 @@
       <h2 class="title has-text-centered">Competencias</h2>
       <div class="container is-widescreen">
         <p class="has-text-centered">
-          Comencé a participar desde el pre-universitario en competiciones
-          relacionadas con la programación y la informática, a diferentes
-          niveles y en las que he obtenido resultados satisfactorios.
+          Desde mi etapa preuniversitaria comencé a participar de forma
+          entusiasta en competiciones de programación, copas y torneos
+          nacionales. Obtuve tres años consecutivos(2014-2016) medalla de plata
+          en el Concurso Nacional de Computación. Desde esta etapa me involucré
+          con el torneo para universitarios de ACM-ICPC, donde competía como
+          invitado. Una vez entré a la universidad participé de forma oficial,
+          llegando a la Final Caribeña y obteniendo los lugares 13(2018) y
+          8(2019). Contribuí a que mi universidad se llevara la medalla de
+          bronce en la Liga Cubana de Programación en 2019. También he
+          participado en rondas de La Red de Programación Competitiva y
+          Codeforces.
         </p>
       </div>
     </section>
@@ -31,9 +39,12 @@
       <div class="container is-widescreen">
         <p class="has-text-centered">
           Me gusta participar en todo tipo de eventos en los que pueda aprender
-          algo. En la Universidad de Ciencias informáticas y por el movimiento
-          de programación competitiva tuve la suerte de formar parte de varios
-          campamentos caribeños donde conocí gente entusiasta y muy preparada.
+          algo. En la Universidad de Ciencias Informáticas y por el Movimiento
+          de Programación Competitiva tuve la suerte de formar parte de varios
+          Campamentos Caribeños donde conocí gente entusiasta y muy preparada,
+          eventos conducidos por profesores con basta experiencia como es el
+          caso de Pablo Zimmerman(ARG), Leopoldo Taravilse(ARG), Artzem
+          Zuck(RUS).
         </p>
       </div>
     </section>
