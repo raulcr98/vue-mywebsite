7087d55dccc51a8209629fec6adc1c0bc532e60c
Raul C. Rivero <rahulrcr98@gmail.com>
2020-06-23 00:45:54 -0400

    perf: Improved index structure; Removed unused components

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,33 @@
 <template>
   <div class="container margin-top-20">
-    <Banner />
+    <div id="my-banner" class="hero is-white">
+      <div class="hero-body">
+        <div id="center" class="container">
+          <h1 class="title is-1">Raúl C. Rivero</h1>
+          <img class="photo" src="banner1.jpg" alt="" />
+          <p class="pt">
+            Hola mi nombre es Raúl y soy desarrollador web. Puedes leer más
+            <a href="/about/">sobre mí</a>.
+          </p>
+          <div class="pt">
+            <a v-for="social in socials" :key="social.name" :href="social.url">
+              <span class="mr icon is-small">
+                <b-icon :icon="social.name"></b-icon>
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Banner from '~/components/Banner.vue'
-
 export default {
-  components: { Banner }
+  data() {
+    return {
+      socials: require('~/data/social.json')
+    }
+  }
 }
 </script>
-
-<style></style>
