c034f6e9b9a1d6a951e6f1fe1e27e558c319b6f4
Raul C. Rivero <rahulrcr98@gmail.com>
2020-10-13 18:22:36 -0400

    improve seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,9 +93,6 @@ export default {
   modules: [
     // Doc: https://buefy.github.io/#/documentation
     'nuxt-buefy',
-    ['@nuxtjs/google-adsense', {
-      id: 'ca-pub-6595440746241172'
-    }],
 
     '@nuxtjs/axios',
 
